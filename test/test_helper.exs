1c6e89dd315f684a3ef1075e34746ef9cf498431
Luis Sagastume <8935075+sagastume@users.noreply.github.com>
2024-03-25 12:42:53 -0600

    feat: Add docker-compose and helper for defaults opts

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,20 @@
 ExUnit.start()
+
+defmodule BoltxEcto.TestHelper do
+  def opts() do
+    [
+      hostname: "127.0.0.1",
+      auth: [username: "neo4j", password: "boltxPassword"],
+      user_agent: "boltxTest/1",
+      ssl_opts: ssl_opts(),
+      pool_size: 1,
+      prefix: :default,
+      scheme: "bolt",
+      ssl: false
+    ]
+  end
+
+  defp ssl_opts() do
+    [versions: [:"tlsv1.2"]]
+  end
+end
